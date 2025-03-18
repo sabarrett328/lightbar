195723f0a51a1cbc583d42bdd8ba0098b38c841b
Scott Barrett <45408775+sabarrett328@users.noreply.github.com>
2025-03-18 11:53:00 -0400

    Create code.py
    1st try.  Need to run

---
+++ code.py
@@ -0,0 +1,52 @@
+import board
+import digitalio
+import rotaryio
+import pwmio
+
+# Constants
+PWM_FREQ = 1000  # PWM frequency in Hz
+PWM_MAX = 65535  # Maximum PWM duty cycle
+PWM_HALF = PWM_MAX // 2  # 50% brightness step
+ENCODER_STEP = 1000  # Step size for brightness adjustment
+
+# Define PWM outputs for LED strings
+led1 = pwmio.PWMOut(board.D5, frequency=PWM_FREQ, duty_cycle=0)
+led2 = pwmio.PWMOut(board.D6, frequency=PWM_FREQ, duty_cycle=0)
+
+# Rotary encoders
+encoder1 = rotaryio.IncrementalEncoder(board.D2, board.D3)
+encoder2 = rotaryio.IncrementalEncoder(board.D4, board.D7)
+
+# Encoder pushbuttons
+button1 = digitalio.DigitalInOut(board.D8)
+button1.switch_to_input(pull=digitalio.Pull.UP)
+button2 = digitalio.DigitalInOut(board.D9)
+button2.switch_to_input(pull=digitalio.Pull.UP)
+
+# State variables
+brightness = {"led1": 0, "led2": 0}
+toggle_state = {"led1": False, "led2": False}
+last_position = {"encoder1": encoder1.position, "encoder2": encoder2.position}
+
+# Function to update brightness
+def update_brightness(encoder, last_pos, led, key):
+    position = encoder.position
+    if position != last_pos:
+        brightness[key] = max(0, min(PWM_MAX, brightness[key] + (position - last_pos) * ENCODER_STEP))
+        led.duty_cycle = brightness[key]
+    return position
+
+# Function to handle button press
+def handle_button(button, led, key):
+    if not button.value:
+        while not button.value:
+            pass  # Debounce
+        toggle_state[key] = not toggle_state[key]
+        led.duty_cycle = PWM_HALF if toggle_state[key] else 0
+
+while True:
+    last_position["encoder1"] = update_brightness(encoder1, last_position["encoder1"], led1, "led1")
+    last_position["encoder2"] = update_brightness(encoder2, last_position["encoder2"], led2, "led2")
+    
+    handle_button(button1, led1, "led1")
+    handle_button(button2, led2, "led2")
